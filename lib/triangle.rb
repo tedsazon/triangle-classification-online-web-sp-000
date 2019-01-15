8a018b32a469172872a74e3b5d5d0b14f40c85df
Ted Sazon <tedsazon@gmail.com>
2019-01-15 16:00:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     if side1 == side2 && side2 == side3
        :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
-      puts :isosceles
+       :isosceles
     elsif side1 != side2 && side2 != side3
       puts :scalene
     end
